8f5e5d97100f24b34f3877f7eb553bd6c3b58d5d
RomanIgambaev <roman.igambaev11@gmail.com>
2023-09-13 16:46:36 +0500

    ValidParentheses

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+    static boolean chekingValidParentheses(String s) {
+        int left = 0;
+        int right = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                left++;
+            } else if (c == ')') {
+                right++;
+            } else {
+                System.out.println("Можно вводить только скобки");
+                continue;
+            }
+
+            if (right > left || right < left) {
+                return false;
+            }
+        }
+
+        return left == right;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку с круглыми скобками: ");
+        String input = scanner.nextLine();
+        boolean isValid = chekingValidParentheses(input);
+        System.out.println("Результат проверки: " + isValid);
     }
 }
